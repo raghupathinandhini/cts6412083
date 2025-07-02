99ce0848ef5b86647d987c53e0b320ecd9657843
raghupathinandhini <156089964+raghupathinandhini@users.noreply.github.com>
2025-07-02 17:45:19 +0530

    Create MyTest.java

---
+++ week-2/JUnit_Basic Testing Exercises/Setting Up JUnit/MyTest.java
@@ -0,0 +1,11 @@
+	package com.google.MavenProjects; 
+
+	import static org.junit.Assert.assertEquals;
+	import org.junit.Test;
+
+	public class MyTest {
+	    @Test
+	    public void testAddition() {
+	        assertEquals(5, 2 + 3);
+	    }
+}
