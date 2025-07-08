055087bf3843e446405185c4f2850c1b10016370
raghupathinandhini <156089964+raghupathinandhini@users.noreply.github.com>
2025-07-08 17:44:56 +0530

    Create LibraryManagementApplication.java

---
+++ week3/Spring Core_Maven/Exercise 4: Creating and Configuring a Maven Project/LibraryManagementApplication.java
@@ -0,0 +1,7 @@
+package com.example.library;
+
+public class LibraryManagementApplication {
+	    public static void main(String[] args) {
+	        System.out.println(" Maven Project is set up successfully with Spring dependencies!");
+	    }
+}
