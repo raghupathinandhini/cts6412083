103f4657a18b370140a090ef7856db936132ebd2
raghupathinandhini <156089964+raghupathinandhini@users.noreply.github.com>
2025-07-15 17:58:00 +0530

    Employee.java

--- week3/Spring Data JPA with Spring Boot, Hibernate/Difference between JPA,Hibernate and spring data JPA/Spring data JPA example/Employee.java
+++ week3/Spring Data JPA with Spring Boot, Hibernate/Difference between JPA,Hibernate and spring data JPA/Spring data JPA example/Employee.java
@@ -10,4 +10,20 @@ public class Employee {
     private String name;
 
     // getters and setters
+    public int getId() {
+    return id;
+}
+
+public void setId(int id) {
+    this.id = id;
+}
+
+public String getName() {
+    return name;
+}
+
+public void setName(String name) {
+    this.name = name;
+}
+    
 }
